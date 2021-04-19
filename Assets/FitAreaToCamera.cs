ae83b4e4089bb0c9b68365d79b4f2ef429d4b000
Alexandre Bourse <Alexandre.bourse@gmail.com>
2021-04-19 11:56:44 +0200

    Chapter 04.07

--- Assets/FitAreaToCamera.cs
+++ Assets/FitAreaToCamera.cs
@@ -1,43 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 /// <summary>
-/// Fit area to camera
+/// Fit area to camera.
 /// </summary>
-
-
-[AddComponentMenu("Banquise/Fit Area To Camera")]
+[AddComponentMenu ("Jikkou Publishing/Fit Area to Camera")]
 [RequireComponent (typeof (GameArea))]
 public class FitAreaToCamera : MonoBehaviour
 {
-    private GameArea Area
-    {
-        get { return GetComponent<GameArea>(); }
-    }
+	private GameArea Area
+	{
+		get { return GetComponent<GameArea>(); }
+	}
 
-    private void Awake()
-    {
-        FitToMainCamera();
-    }
+	private void Awake ()
+	{
+		FitToMainCamera();
+	}
 
-    private void FitToCamera (Camera cam)
-    {
-        //Area.SetArea(new Vector2 (cam.aspect * cam.orthographicSize * 2, cam.orthographicSize * 2));
-        Area.Size = new Vector2 (cam.aspect * cam.orthographicSize * 2, cam.orthographicSize * 2);
-        transform.position = (Vector2)cam.transform.position;
-        transform.rotation = cam.transform.rotation;
-    }
+	private void FitToCamera (Camera cam)
+	{
+		Area.Size = new Vector2 (cam.aspect * cam.orthographicSize * 2, cam.orthographicSize * 2);
+		transform.position = (Vector2)cam.transform.position;
+		transform.rotation = cam.transform.rotation;
+	}
 
-    private void FitToMainCamera()
-    {
-        FitToCamera(Camera.main);
-    }
+	private void FitToMainCamera ()
+	{
+		FitToCamera(Camera.main);
+	}
 
-    private void Reset()
-    {
-        FitToMainCamera();
-    }
-
-  
+	private void Reset ()
+	{
+		FitToMainCamera();
+	}
 }
--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -1,131 +1,102 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class Spawner : MonoBehaviour
 {
+	[Header ("SPAWN")]
+	public GameObject reference;
 
-    [Header("SPAWN")]
-    public GameObject reference;
-    [Header("SPAWNING")]
-    [Range(0.001f,100f)] public float minrate = 1.0f;
-    [Range(0.001f,100f)] public float maxrate = 1.0f;
-    public int number = 5;
-    public bool infinite; 
+	[Header ("SPAWNING")]
+	[Range (0.001f, 100f)] public float minRate = 1.0f;
+	[Range (0.001f, 100f)] public float maxRate = 1.0f;
+	public int number = 5;
+	public bool infinite;
 
-    private int _remaining;
-    private float _timeStamp;
+	private int _remaining;
 
-    [Header("LOCATIONS")]
-    public GameArea area;
-    private Transform player;
-    public float minDistanceFromPlayer;
+	[Header ("LOCATIONS")]
+	public GameArea area;
+	private Transform player;
+	public float minDistanceFromPlayer;
 
-    [Header("VELOCITY")]
-    [Range(-180f, 180f)] public float angle;
-    [Range(0f, 360f)]public float spread = 30f;
-    [Range(0, 10)] public float minStrength = 1f;
-    [Range(0, 10)] public float maxStrengh = 10f;
+	[Header ("VELOCITY")]
+	[Range (-180f, 180f)] public float angle;
+	[Range (0, 360f)] public float spread = 30f;
+	[Range (0, 10)] public float minStrength = 1f;
+	[Range (0, 10)] public float maxStrength = 10f;
 
+	[Header ("ANIMATOR")]
+	public string animatorSpawningParameterName = "Spawning";
+	public float animatorDelayIn = 1;
+	public float animatorDelayOut = 1;
 
-    [Header("ANIMATOR")]
-    public string animatorSpawningParameterName = "spawning";
-    private int spawningHashId;
-    public float animatorDelayIn = 1;
-    public float animatorDelayOut = 1;
+	private Animator animator;
+	private int spawningHashID;
 
+	void Awake ()
+	{
+		animator = GetComponent<Animator>();
+		if (animator)
+			spawningHashID = Animator.StringToHash(animatorSpawningParameterName);
+	}
 
-    private Animator animator;
+	private IEnumerator Start ()
+	{
+		_remaining = number;
 
-    // Start is called before the first frame update
-    //void Start()
-    //{
-    //    _remaining = number;
-    //    StartCoroutine(Spawn());
-    //    //_timeStamp = Time.time;
-    //}
+		if (minDistanceFromPlayer > 0)
+		{
+			GameObject playerGO = GameObject.FindGameObjectWithTag("Player");
+			if (playerGO)
+				player = playerGO.transform;
+			else
+				Debug.LogWarning("No Player Found.");
+		}
 
-    // Update is called once per frame
-    //void Update()
-    //{
-    //    if (Time.time <= _timeStamp + rate)
-    //        return;
+		if (animator)
+		{
+			animator.SetBool(spawningHashID, true);
+			yield return new WaitForSeconds(animatorDelayIn);
+		}
 
-    //    _timeStamp = Time.time;
+		while (infinite || _remaining > 0)
+		{
+			Vector3 _position = area ? area.GetRandomPosition() : transform.position;
 
-    //    //if(_remaining > 0) 
-    //    //{ 
-    //        Instantiate(reference, transform.position, transform.rotation);
-    //        _remaining--;
-    //    //}
-    //    if (!infinit && _remaining <= 0)
-    //        this.enabled = false;  
-    //}
+			if (player && Vector3.Distance(_position, player.position) < minDistanceFromPlayer)
+			{
+//				Debug.Log(_position);
+				Vector3 debugPos = _position;
+				Debug.DrawLine(transform.position, debugPos);
+				_position = (_position-player.position).normalized * minDistanceFromPlayer;
+				Debug.DrawLine(debugPos, _position);
+//				Debug.Break();
+			}
 
+			// TODO : Use Object Pooling
+			GameObject obj = Instantiate(reference, _position, transform.rotation);
 
-    private void Awake()
-    {
-        animator = GetComponent<Animator>();
-        if (animator)
-        {
-            spawningHashId = Animator.StringToHash(animatorSpawningParameterName);
-        }
-        
-    }
+			Rigidbody2D rb2d = obj.GetComponent<Rigidbody2D>();
+			if (rb2d)
+			{
+				float angleDelta = Random.Range(-spread*0.5f, spread*0.5f);
+				float angle_ = angle + angleDelta;
+				Vector2 direction = new Vector2 (Mathf.Sin(Mathf.Deg2Rad*angle_), Mathf.Cos(Mathf.Deg2Rad*angle_));
+				direction *= Random.Range(minStrength, maxStrength);
+				rb2d.velocity = direction;
+			}
 
-    private IEnumerator Start()
-    {
+			_remaining --;
 
-        if (animator)
-        {
-            animator.SetBool(animatorSpawningParameterName, true);
-            yield return new WaitForSeconds(animatorDelayIn);
-        }
-        _remaining = number;
+			yield return new WaitForSeconds(1/Random.Range(minRate, maxRate));
+		}
 
-        if (minDistanceFromPlayer > 0) { 
-        GameObject playerGO = GameObject.FindGameObjectWithTag("Player");
-            if (playerGO)
-            {
-                player = playerGO.transform;
-            }else
-            {
-                Debug.LogWarning("No player found");
-            }
-        }
-       
-        while (infinite || _remaining > 0)
-        {
-            //Vector3 _position = area.GetRandomPosition();
-            Vector3 _position = area ? area.GetRandomPosition() : transform.position;
-            if(player &&  Vector3.Distance(_position, player.position) < minDistanceFromPlayer){
-                Vector3 debugPos = _position; 
-                Debug.DrawLine(transform.position, debugPos);
-                _position = (_position - player.position).normalized * minDistanceFromPlayer;
-                Debug.DrawLine(debugPos, _position);
-                //Debug.Break();
-            }
+		if (animator)
+		{
+			yield return new WaitForSeconds(animatorDelayOut);
+			animator.SetBool(spawningHashID, false);
+		}
 
-            GameObject obj =  Instantiate(reference, _position, transform.rotation) as GameObject;
-            Rigidbody2D rb2d = obj.GetComponent<Rigidbody2D>();
-            if (rb2d)
-            {
-                float angleDelta = Random.Range(-spread * 0.5f, spread * 0.5f);
-                float angle_ = angle + angleDelta;
-                Vector2 direction = new Vector2(Mathf.Sin(Mathf.Deg2Rad*angle_), Mathf.Cos(Mathf.Deg2Rad*angle_));
-                direction *= Random.Range(minStrength, maxStrengh);
-                rb2d.velocity = direction;
-            }
-
-            _remaining--;
-
-            yield return new WaitForSeconds(1 / Random.Range(minrate,maxrate));
-        }
-        
-        if (animator)
-        {
-            yield return new WaitForSeconds(animatorDelayOut);
-            animator.SetBool(animatorSpawningParameterName, false);
-        }
-    }
+		gameObject.SetActive(false);
+	}
 }
